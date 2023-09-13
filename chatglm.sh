aa2e507a1401e06ecfc8f98399b31f4e90a6af13
佘明东 <scg130@163.com>
2023-09-13 14:23:09 +0800

    Create chatglm.sh

---
+++ chatglm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git clone https://github.com/THUDM/ChatGLM2-6B
+cd ChatGLM2-6B
+pip install -r requirements.txt
+pip install pyqt5==5.15
+pip install pyqtwebengine==5.15
+#模型下载   https://cloud.tsinghua.edu.cn/d/674208019e314311ab5c/
+
+# web_demo.py
+# tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b-int4", revision="v1.0",trust_remote_code=True)
+# float() 使用cpu   cuda()  使用gpu
+# model = AutoModel.from_pretrained("THUDM/chatglm2-6b-int4", trust_remote_code=True).float()
+# demo.queue().launch(share=True, inbrowser=True,server_port=6543,server_name="0.0.0.0")
