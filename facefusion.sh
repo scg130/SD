e7db1c02b3a3a193ff8a42f0ed57af3f080a04b1
MingDong She <scg130@163.com>
2023-10-05 02:41:16 +0800

    so-vtis-svc

--- facefusion.sh
+++ facefusion.sh
@@ -4,3 +4,5 @@ git clone https://github.com/facefusion/facefusion.git
 文件在百度网盘下载
 将facefusion.rar解压到根目录
 将.insightface解压到 C:\Users\用户名
+
+链接: https://pan.baidu.com/s/13fmnFCBVYpiuyJNcW3t2uw 提取码: 62ew 
---
+++ so-vits-scg.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+链接: https://pan.baidu.com/s/1VxLlZjMsOV1dJtir7Wg94A 提取码: sef2 
