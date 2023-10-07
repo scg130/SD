8e2f106772997c2fc152cfd8e35752f4c83479e0
scg130 <scg130@163.com>
2023-10-07 00:31:13 -0400

    clash

---
+++ clash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /usr/local/src
+mkdir -p clash
+cd clash/
+wget https://github.com/Dreamacro/clash/releases/download/v1.11.12/clash-linux-amd64-v1.11.12.gz
+gzip -d clash-linux-amd64-v1.11.12.gz
+mv clash-linux-amd64-v1.11.12 clash
+chmod -R 755 clash
+wget -o config.yaml "配置文件的 url"
+wget "https://raw.githubusercontent.com/wp-statistics/GeoLite2-Country/master/GeoLite2-Country.mmdb.gz"
+gzip -d GeoLite2-Country.mmdb.gz
+mv GeoLite2-Country.mmdb Country.mmdb
+chmod -R 755 ./*
+#./clash -d .
