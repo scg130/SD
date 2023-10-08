063fd2f8fc3f0fa63dfcd8d9db48f5f518b7e629
佘明东 <scg130@163.com>
2023-10-08 11:54:07 +0800

    Update clash.sh

--- clash.sh
+++ clash.sh
@@ -6,7 +6,7 @@ wget https://github.com/Dreamacro/clash/releases/download/v1.11.12/clash-linux-a
 gzip -d clash-linux-amd64-v1.11.12.gz
 mv clash-linux-amd64-v1.11.12 clash
 chmod -R 755 clash
-wget -o config.yaml "https://connect.applecross.link/clash/862983/i2Wtg2RNGJpZ"
+wget -O config.yaml "https://connect.applecross.link/clash/862983/i2Wtg2RNGJpZ"
 wget "https://raw.githubusercontent.com/wp-statistics/GeoLite2-Country/master/GeoLite2-Country.mmdb.gz"
 gzip -d GeoLite2-Country.mmdb.gz
 mv GeoLite2-Country.mmdb Country.mmdb
