76c0c019954d0664e514ff302a7c3c3ab386f151
scg130 <scg130@163.com>
2023-10-11 11:59:14 +0800

    1

--- facefusion.sh
+++ facefusion.sh
@@ -2,7 +2,7 @@
 #换脸软件 facefusion
 git clone https://github.com/facefusion/facefusion.git
 文件在百度网盘下载
-将facefusion.rar解压到根目录
+将facefusion.rar解压到项目根目录
 将.insightface解压到 C:\Users\用户名
 
 链接: https://pan.baidu.com/s/13fmnFCBVYpiuyJNcW3t2uw 提取码: 62ew 
