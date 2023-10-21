5d69c69c4df296bf11dfa0da33fca0c3fa102076
MingDong She <scg130@163.com>
2023-10-21 17:37:34 +0800

    download

--- dataset_download.sh
+++ dataset_download.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/ee66-00b7de2d1056dd605b4d2a6926893245.jpg/w700d1q75cms.jpg
+wget -O 1.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/ee66-00b7de2d1056dd605b4d2a6926893245.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/0716-415109e5be1c2c7856a6e50c7281a402.jpg/w700d1q75cms.jpg
+wget -O 2.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/0716-415109e5be1c2c7856a6e50c7281a402.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/8422-ed21d692d0a7d7d8873ef1b46422a1a6.jpg/w700d1q75cms.jpg
+wget -O 3.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/8422-ed21d692d0a7d7d8873ef1b46422a1a6.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/f5d9-aad266f95b3c4c81d14f8d48685ad209.jpg/w700d1q75cms.jpg
+wget -O 4.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/f5d9-aad266f95b3c4c81d14f8d48685ad209.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/2daa-ea18d268f46a041cd4ca3d22081feccb.jpg/w700d1q75cms.jpg
+wget -O 5.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/2daa-ea18d268f46a041cd4ca3d22081feccb.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/928b-b949432f81ded19a84e7f31a42b08263.jpg/w700d1q75cms.jpg
+wget -O 6.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/928b-b949432f81ded19a84e7f31a42b08263.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/d861-6d60b081886d89af214f5bc6197dab95.jpg/w700d1q75cms.jpg
+wget -O 7.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/d861-6d60b081886d89af214f5bc6197dab95.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/7fea-21424a3fb5e2274bfab87b0d779afad8.jpg/w700d1q75cms.jpg
+wget -O 8.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/7fea-21424a3fb5e2274bfab87b0d779afad8.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/22dd-748627f0a449211af23b54cdbea591d4.jpg/w700d1q75cms.jpg
+wget -O 9.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/22dd-748627f0a449211af23b54cdbea591d4.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/1679-2ccf077b3123778c5ba1539d226d3b6e.jpg/w700d1q75cms.jpg
+wget -O 10.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/1679-2ccf077b3123778c5ba1539d226d3b6e.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/2f1b-94d351d2d0bb1711a9f6f93991dcdd60.jpg/w700d1q75cms.jpg
+wget -O 11.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/2f1b-94d351d2d0bb1711a9f6f93991dcdd60.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/a888-c291dc51fa3e5155360930de1fa1e886.jpg/w700d1q75cms.jpg
+wget -O 12.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/a888-c291dc51fa3e5155360930de1fa1e886.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/fec3-f92428de634735898bd4c5f231cd91d0.jpg/w700d1q75cms.jpg
+wget -O 13.jpg  http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/fec3-f92428de634735898bd4c5f231cd91d0.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/876e-ef99676498add477585effa5fe6d9bc8.jpg/w700d1q75cms.jpg
+wget -O 14.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/876e-ef99676498add477585effa5fe6d9bc8.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/2801-f0672fb91aac0f04ddcd1a27d5e08986.jpg/w700d1q75cms.jpg
+wget -O 15.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/2801-f0672fb91aac0f04ddcd1a27d5e08986.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/cf8e-148e2cf8ca41e06cff24bd8ad8b2dd7f.jpg/w700d1q75cms.jpg
+wget -O 16.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/cf8e-148e2cf8ca41e06cff24bd8ad8b2dd7f.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/92e9-7aea9e91f78e3c2414d6ae19ada35c94.jpg/w700d1q75cms.jpg
+wget -O 17.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/92e9-7aea9e91f78e3c2414d6ae19ada35c94.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/381f-fce76ac1db0b5e418187533f771ccfd0.jpg/w700d1q75cms.jpg
+wget -O 18.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/381f-fce76ac1db0b5e418187533f771ccfd0.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/560d-32b0595c312cd9b4576dbf62ce0c162b.jpg/w700d1q75cms.jpg
+wget -O 19.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/560d-32b0595c312cd9b4576dbf62ce0c162b.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/e11e-45f8ecb0c0bba8ec71ea935de07a316c.jpg/w700d1q75cms.jpg
+wget -O 20.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/e11e-45f8ecb0c0bba8ec71ea935de07a316c.jpg/w700d1q75cms.jpg
 
-wget http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/a1f6-925d47519fd4d8a3d6e25abe143662d1.jpg/w700d1q75cms.jpg
+wget -O 21.jpg http://k.sinaimg.cn/n/sinakd20116/300/w1080h1620/20231001/a1f6-925d47519fd4d8a3d6e25abe143662d1.jpg/w700d1q75cms.jpg
