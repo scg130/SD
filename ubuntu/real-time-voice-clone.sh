f44c82134c152fe9514e65ceb6fdf53e1690e9d5
佘明东 <scg130@163.com>
2024-09-28 03:23:54 +0800

    Create real-time-voice-clone.sh

---
+++ ubuntu/real-time-voice-clone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone https://github.com/CorentinJ/Real-Time-Voice-Cloning.git
+cd Real-Time-Voice-Cloning/
+python -m pip install --upgrade pip
+python -m venv venv
+source venv/bin/activate
+sudo apt-get update
+sudo apt-get install -y libsndfile1-dev
+pip install torch
+apt-get install -y ffmpeg 
+mkdir -p saved_models/default
